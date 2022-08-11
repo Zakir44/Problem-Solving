1c6d1f298b8c5bfef8eeb6f8c534a1cd4f683956
Zakir44 <khjakirahmed3909@gmail.com>
2022-08-11 08:53:12 +0600

    Add

---
+++ gap-even.js
@@ -0,0 +1,12 @@
+function evenCheck()
+{
+    for(let i = 7; i <= 19; i++)
+{
+    if(i % 2 ==1)
+    {
+    console.log("This is even number")
+    }
+}
+}
+const evenNumbers = evenCheck()
+console.log(even)
---
+++ tempareture.js
@@ -0,0 +1,5 @@
+function cTof(number)
+{
+
+}
+const result =cTof(78)
